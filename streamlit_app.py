bf1a0531b62a45283651f5d1afce7ea2d1d72a0e
cristian412 <cristianamarillacloss@gmail.com>
2025-01-06 19:50:52 +0000

    beta 06.07

--- streamlit_app.py
+++ streamlit_app.py
@@ -223,7 +223,8 @@ if driver:
                 # Abrir el archivo y enviarlo como parte del POST
                 with open(pdf_path, "rb") as pdf_file:
                     files = {"file": (f"{juicio}-tasa.pdf", pdf_file, "application/pdf")}
-                    response = requests.post(upload_url, files=files)
+                    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+                    response = requests.post(upload_url, files=files, headers=headers)
                 # Verificar la respuesta del servidor
                 if response.status_code == 200:
                     st.success(f"✅ Archivo enviado con éxito al servidor.")
